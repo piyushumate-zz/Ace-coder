1700df043695ce24fd67f264052f3a66e361c5d4
piyushumate <piyush.umate@gmail.com>
2014-09-11 03:20:37 +0530

    First Commit-without input

---
+++ sample.js
@@ -0,0 +1,18 @@
+/*
+// Below is a sample code to process input from STDIN.
+// Equivalent in effect to the Java declaration import java.io.*;
+importPackage(java.io);
+importPackage(java.lang);
+importPackage(java.math);
+importPackage(java.util);
+
+var sc = new Scanner(System['in']);
+
+var input = sc.nextInt();
+System.out.println(input);
+System.out.println('Hello, there!\n');
+// End of input processing code.
+*/
+
+print ('Hello World!\n');
+
